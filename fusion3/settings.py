018316ab8846ff2ab9459ef7a0dd1ee92c960ae4
Henrique-Marmiroli <lhm.ambiental@gmail.com>
2021-11-28 16:16:24 -0300

    Arquivos banco de dados

--- fusion3/settings.py
+++ fusion3/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,6 +87,11 @@ DATABASES = {
     }
 }
 
+"""
+DATABASES = {
+    'default': dj_database_url.config()
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -143,3 +149,5 @@ DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'
